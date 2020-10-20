86e60fe23ac0a3b818ddd0e19b6ac15155af0988
mmx369 <maksim@klishevich.com>
2020-10-21 00:08:36 +0300

    new

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,18 +1,13 @@
 import React from 'react'
+import Part from './Part'
 
-const Content = ({part1, part2, part3, exercises1, exercises2, exercises3}) => {
+const Content = ({ part1, part2, part3, exercises1, exercises2, exercises3 }) => {
     return (
-    <div>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      </div>)
+        <div>
+            <Part part={part1} exersise={exercises1} />
+            <Part part={part2} exersise={exercises2} />
+            <Part part={part3} exersise={exercises3} />
+        </div>)
 }
 
 export default Content
---
+++ src/components/Part.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Part = ({ part, exersise }) => <p>{part} {exersise}</p>
+
+export default Part
